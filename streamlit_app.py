212fd9782681da5d7f0a8f2d427a0b7be02f026c
Jose Adrian de Dios <117702634+king-joseadrian@users.noreply.github.com>
2022-11-08 12:01:24 +0800

    Update streamlit_app.py
    added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,9 @@
 import streamlit
 
 streamlit.title('My Parents new Healthy Diner')
+
+
+streamlit.head('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
