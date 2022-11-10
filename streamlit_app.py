2a110f8c1e129409496f6d1ea9032b9c42b42c53
Jose Adrian de Dios <117702634+king-joseadrian@users.noreply.github.com>
2022-11-10 11:29:23 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,18 +24,26 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice api response
+
+
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?)
+   if not fruit_choice:
+      streamlit.error("Please select a fruit to get information.")
+    else
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+                                       
+ except ERLError as e:
+ streamlit.error()
+ #streamlit.write('The user entered ', fruit_choice)
 #import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
 # use pandas libraries to normalise and put json data in table ??
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # display table???
-streamlit.dataframe(fruityvice_normalized)
+
 
 # dont run anything after this
 streamlit.stop()
