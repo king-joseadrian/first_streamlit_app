94c795affb8a826c0c9831cfa60b28f54ffd16d3
Jose Adrian de Dios <117702634+king-joseadrian@users.noreply.github.com>
2022-11-10 13:34:11 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ try:
       back_from_function=get_fruity_data(fruit_choice)
       streamlit.dataframe(back_from_function)
                                                
-except ERLError as e:
+except URLError as e:
 #   streamlit.error()
 #streamlit.write('The user entered ', fruit_choice)
 #import requests
